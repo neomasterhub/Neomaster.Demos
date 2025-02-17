41c5fd9945245332d5dafd5e7d925d501818fda2
Neomaster X <neomasterx@outlook.com>
2025-02-17 08:36:56 +0300

    [Threads] Improve AbortThreadInCoreViaCancellationToken()

--- Neomaster.Demos.Tests/Threads/ThreadsUnitDemos.cs
+++ Neomaster.Demos.Tests/Threads/ThreadsUnitDemos.cs
@@ -119,21 +119,27 @@ public class ThreadsUnitDemos
   [Fact]
   public void AbortThreadInCoreViaCancellationToken()
   {
-    var count = 0;
+    var cancelled = false;
     var cts = new CancellationTokenSource();
-    var thread = new Thread(() =>
+    var th1 = new Thread(() =>
     {
       while (!cts.IsCancellationRequested)
       {
-        count++;
         Thread.Sleep(10);
       }
+
+      cancelled = true;
+    });
+    var th2 = new Thread(() =>
+    {
+      Thread.Sleep(100);
+      cts.Cancel();
     });
 
-    thread.Start();
-    Thread.Sleep(100);
-    cts.Cancel();
+    th1.Start();
+    th2.Start();
+    th1.Join();
 
-    Assert.True(count > 0 && count < 200);
+    Assert.True(cancelled);
   }
 }
