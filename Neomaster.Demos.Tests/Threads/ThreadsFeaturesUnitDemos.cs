9099cdbb1b17cc18a477f8037312edb84a96df8e
Neomaster X <neomasterx@outlook.com>
2025-03-14 17:37:55 +0300

    [Threads] SynchronizedMethodStaticInDifferentThreads()

--- Neomaster.Demos.Tests/Threads/ThreadsFeaturesUnitDemos.cs
+++ Neomaster.Demos.Tests/Threads/ThreadsFeaturesUnitDemos.cs
@@ -82,6 +82,49 @@ public class ThreadsFeaturesUnitDemos
     Assert.Equal(expectedChars2String, chars2String);
   }
 
+  [Fact(DisplayName = "Synchronized method: static in different threads")]
+  public void SynchronizedMethodStaticInDifferentThreads()
+  {
+    var chars = new ConcurrentQueue<char>();
+    const string expectedChars1String = "AAAAABBBBBCCCCCDDDDD";
+    const string expectedChars2String = "11111222223333344444";
+    const int charNumber = 5;
+    var th1 = new Thread(() =>
+    {
+      for (char c = 'A'; c <= 'D'; c++)
+      {
+        SynchronizedMethodClass.CountStatic(chars, charNumber, c);
+      }
+    });
+    var th2 = new Thread(() =>
+    {
+      for (char c = '1'; c <= '4'; c++)
+      {
+        SynchronizedMethodClass.CountStatic(chars, charNumber, c);
+      }
+    });
+
+    th1.Start();
+    th2.Start();
+    th1.Join();
+    th2.Join();
+
+    var charsString = string.Concat(chars);
+    var uniqueChars = chars.Distinct();
+
+    // Demonstrates that the object being locked is the instance of "typeof(SynchronizedMethodClass)".
+    foreach (var uc in uniqueChars)
+    {
+      var ucSeq = new string(uc, charNumber);
+      Assert.Contains(ucSeq, charsString);
+    }
+
+    var chars1String = string.Concat(chars.Where(c => c >= 'A'));
+    var chars2String = string.Concat(chars.Where(c => c < 'A'));
+    Assert.Equal(expectedChars1String, chars1String);
+    Assert.Equal(expectedChars2String, chars2String);
+  }
+
   public class SynchronizedMethodClass
   {
     [MethodImpl(MethodImplOptions.Synchronized)]
