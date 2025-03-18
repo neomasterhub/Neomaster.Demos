692c4e0d4e1956150fea6eb323fa73921474a8ee
Neomaster X <neomasterx@outlook.com>
2025-03-18 09:27:09 +0300

    ReadmeShouldContainReferencesToTestMethods()

--- Neomaster.Demos.Tests/SolutionTests.cs
+++ Neomaster.Demos.Tests/SolutionTests.cs
@@ -1,3 +1,6 @@
+using System.Text.RegularExpressions;
+using Xunit;
+
 namespace Neomaster.Demos.Tests;
 
 public class SolutionTests
@@ -13,5 +16,44 @@ public class SolutionTests
       0, Environment.CurrentDirectory.IndexOf(solutionName) + solutionName.Length);
 
     _readmePath = Path.Combine(_solutionPath, "readme.md");
+  }
+
+  [Fact]
+  public void ReadmeShouldContainReferencesToTestMethods()
+  {
+    var testFileLineIndexes = File.ReadAllLines(_readmePath)
+      .Where(line => Regex.IsMatch(line, @"#L[0-9]+$"))
+      .Select(line =>
+      {
+        var parts = line.Split("#L");
+        var test = new
+        {
+          FileName = parts[0].Substring(parts[0].LastIndexOf('/') + 1),
+          MethodLineIndex = int.Parse(parts[1]),
+        };
+
+        return test;
+      })
+      .GroupBy(t => t.FileName, t => t.MethodLineIndex, (k, v) => new
+      {
+        FileName = k,
+        MethodLineIndexes = v,
+      });
+
+    var unitDemoFileInfos = new DirectoryInfo(_solutionPath)
+      .EnumerateFiles("*UnitDemos.cs", SearchOption.AllDirectories);
+
+    foreach (var udFi in unitDemoFileInfos)
+    {
+      var udLines = File.ReadAllLines(udFi.FullName);
+      var testLineIndexes = testFileLineIndexes
+        .Single(x => x.FileName == udFi.Name).MethodLineIndexes;
+
+      foreach (var i in testLineIndexes)
+      {
+        var udLine = udLines[i - 1];
+        Assert.Matches(@"^\s*public (void|async Task)", udLine);
+      }
+    }
   }
 }
