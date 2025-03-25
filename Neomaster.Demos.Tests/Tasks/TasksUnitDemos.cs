89b3e8ffaae86e5326aae4f0d111d29f396ce267
Neomaster X <neomasterx@outlook.com>
2025-03-25 12:04:05 +0300

    [Tasks] ThreadPoolQueueUserWorkItemWithState()

--- Neomaster.Demos.Tests/Tasks/TasksUnitDemos.cs
+++ Neomaster.Demos.Tests/Tasks/TasksUnitDemos.cs
@@ -20,4 +20,23 @@ public class TasksUnitDemos
     Thread.Sleep(200);
     Assert.True(result);
   }
+
+  [Fact]
+  public void ThreadPoolQueueUserWorkItemWithState()
+  {
+    var result = 0;
+
+    var queued = ThreadPool.QueueUserWorkItem(
+      state =>
+      {
+        Thread.Sleep(100);
+        result = (int)state;
+      },
+      1);
+
+    Assert.True(queued);
+    Assert.NotEqual(1, result);
+    Thread.Sleep(200);
+    Assert.Equal(1, result);
+  }
 }
