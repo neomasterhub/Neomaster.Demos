1433c55bd45cb2268352f14762888ccd823f32be
Neomaster X <neomasterx@outlook.com>
2025-03-26 15:52:01 +0300

    [Tasks] CreateTask()

--- Neomaster.Demos.Tests/Tasks/TasksUnitDemos.cs
+++ Neomaster.Demos.Tests/Tasks/TasksUnitDemos.cs
@@ -1,5 +1,33 @@
+using Xunit;
+
 namespace Neomaster.Demos.Tests.Tasks;
 
 public class TasksUnitDemos
 {
+  [Fact]
+  public void CreateTask()
+  {
+    var events = new List<int>();
+
+    // Create
+    var t1 = new Task(() =>
+    {
+      Thread.Sleep(100);
+      events.Add(3);
+    });
+    t1.Start();
+
+    // Create and start
+    var t2 = Task.Run(() =>
+    {
+      Thread.Sleep(50);
+      events.Add(2);
+    });
+
+    events.Add(1);
+
+    Thread.Sleep(150);
+
+    Assert.Equal([1, 2, 3], events);
+  }
 }
