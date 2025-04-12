57570e6792a0eb39278a8c3c320d2a4a8782f7cb
Neomaster X <neomasterx@outlook.com>
2025-04-12 11:32:20 +0300

    [OpenXML] AddRows_ToFirstTable()

---
+++ Neomaster.Demos.Tests/Documents/OpenXMLDocTables/TableTemplatesUnitDemos.cs
@@ -0,0 +1,52 @@
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Xunit;
+
+namespace Neomaster.Demos.Tests.Documents.OpenXMLDocTables;
+
+public class TableTemplatesUnitDemos
+{
+  [Fact]
+  public void AddRows_ToFirstTable()
+  {
+    const string fileName = "table-001.docx";
+    var templatePath = GetTemplatePath(fileName);
+    var outputPath = Shared.GetOutputPath(fileName);
+
+    File.Copy(templatePath, outputPath, true);
+
+    using var wordDoc = WordprocessingDocument.Open(outputPath, true);
+
+    var body = wordDoc.MainDocumentPart.Document.Body;
+    var table = body.Elements<Table>().FirstOrDefault();
+    var templateRow = table.Elements<TableRow>().ElementAt(1);
+
+    for (var i = 0; i < 10; i++)
+    {
+      var newRow = (TableRow)templateRow.CloneNode(true);
+
+      foreach (var cell in newRow.Elements<TableCell>())
+      {
+        var text = cell.InnerText
+          .Replace("{{ID}}", i.ToString())
+          .Replace("{{Email}}", Faker.Internet.Email())
+          .Replace("{{First Name}}", Faker.Name.First())
+          .Replace("{{Last Name}}", Faker.Name.Last());
+
+        cell.RemoveAllChildren<Paragraph>();
+        cell.Append(new Paragraph(new Run(new Text(text))));
+      }
+
+      table.AppendChild(newRow);
+    }
+
+    table.RemoveChild(templateRow);
+
+    wordDoc.MainDocumentPart.Document.Save();
+  }
+
+  private static string GetTemplatePath(string fileName)
+  {
+    return Path.Combine("Documents", "OpenXMLDocTables", "Templates", fileName);
+  }
+}
