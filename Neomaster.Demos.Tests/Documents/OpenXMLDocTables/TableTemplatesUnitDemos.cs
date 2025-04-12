8e5dc1ce4b373c959e4e9e7a7480f169bb45345c
Neomaster X <neomasterx@outlook.com>
2025-04-12 12:04:21 +0300

    [OpenXML] AddRows_ToFirstTable_EmailHyperlink()

--- Neomaster.Demos.Tests/Documents/OpenXMLDocTables/TableTemplatesUnitDemos.cs
+++ Neomaster.Demos.Tests/Documents/OpenXMLDocTables/TableTemplatesUnitDemos.cs
@@ -21,7 +21,7 @@ public class TableTemplatesUnitDemos
     var table = body.Elements<Table>().FirstOrDefault();
     var templateRow = table.Elements<TableRow>().ElementAt(1);
 
-    for (var i = 0; i < 10; i++)
+    for (var i = 1; i <= 10; i++)
     {
       var newRow = (TableRow)templateRow.CloneNode(true);
 
@@ -35,6 +35,62 @@ public class TableTemplatesUnitDemos
 
         cell.RemoveAllChildren<Paragraph>();
         cell.Append(new Paragraph(new Run(new Text(text))));
+      }
+
+      table.AppendChild(newRow);
+    }
+
+    table.RemoveChild(templateRow);
+
+    wordDoc.MainDocumentPart.Document.Save();
+  }
+
+  [Fact]
+  public void AddRows_ToFirstTable_EmailHyperlink()
+  {
+    var templatePath = GetTemplatePath("table-001.docx");
+    var outputPath = Shared.GetOutputPath("table-001_email-hyperlink.docx");
+
+    File.Copy(templatePath, outputPath, true);
+
+    using var wordDoc = WordprocessingDocument.Open(outputPath, true);
+
+    var body = wordDoc.MainDocumentPart.Document.Body;
+    var table = body.Elements<Table>().FirstOrDefault();
+    var templateRow = table.Elements<TableRow>().ElementAt(1);
+
+    for (var i = 1; i <= 10; i++)
+    {
+      var newRow = (TableRow)templateRow.CloneNode(true);
+
+      foreach (var cell in newRow.Elements<TableCell>())
+      {
+        var par = new Paragraph();
+        var text = cell.InnerText;
+
+        if (text.Contains("{{Email}}"))
+        {
+          par.Append(
+            OpenXMLTools.CreateEmailHyperlink(
+              wordDoc.MainDocumentPart,
+              Faker.Internet.Email(),
+              new Color { Val = "#0070C0" },
+              new Underline { Val = UnderlineValues.Single }));
+        }
+        else
+        {
+          par.Append(
+            new Run(
+              new Text(
+                text
+                  .Replace("{{ID}}", i.ToString())
+                  .Replace("{{Email}}", Faker.Internet.Email())
+                  .Replace("{{First Name}}", Faker.Name.First())
+                  .Replace("{{Last Name}}", Faker.Name.Last()))));
+        }
+
+        cell.RemoveAllChildren<Paragraph>();
+        cell.Append(par);
       }
 
       table.AppendChild(newRow);
