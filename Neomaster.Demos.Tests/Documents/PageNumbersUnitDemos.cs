eda970c0391dc69f3fa28bc51d5d5ab5860f3ab7
Neomaster X <neomasterx@outlook.com>
2025-04-13 12:14:40 +0300

    #24 [Open XML] PageNumbersUnitDemos.Add_OfTotal() v2

--- Neomaster.Demos.Tests/Documents/PageNumbersUnitDemos.cs
+++ Neomaster.Demos.Tests/Documents/PageNumbersUnitDemos.cs
@@ -48,4 +48,51 @@ public class PageNumbersUnitDemos : OpenXMLUnitDemosBase
 
     wordDoc.Save();
   }
+
+  [Fact]
+  public void Add_OfTotal()
+  {
+    var templatePath = GetTemplatePath("text-001.docx");
+    var outputPath = GetOutputPath(Add_OfTotal);
+
+    File.Copy(templatePath, outputPath, true);
+
+    using var wordDoc = WordprocessingDocument.Open(outputPath, true);
+
+    var mainPart = wordDoc.MainDocumentPart;
+    var footerPart = mainPart.AddNewPart<FooterPart>();
+    var footerPartId = mainPart.GetIdOfPart(footerPart);
+
+    footerPart.Footer = new Footer(
+      new Paragraph(
+        new ParagraphProperties(
+          new Justification() { Val = JustificationValues.Center }),
+        new Run(
+          new FieldChar() { FieldCharType = FieldCharValues.Begin },
+          new FieldCode("PAGE") { Space = SpaceProcessingModeValues.Preserve },
+          new FieldChar() { FieldCharType = FieldCharValues.Separate },
+          new FieldChar() { FieldCharType = FieldCharValues.End },
+          new Text(" / "),
+          new FieldChar() { FieldCharType = FieldCharValues.Begin },
+          new FieldCode("NUMPAGES") { Space = SpaceProcessingModeValues.Preserve },
+          new FieldChar() { FieldCharType = FieldCharValues.Separate },
+          new FieldChar() { FieldCharType = FieldCharValues.End })));
+    footerPart.Footer.Save();
+
+    var sp = mainPart.Document.Body.Elements<SectionProperties>().LastOrDefault();
+    if (sp == null)
+    {
+      sp = new SectionProperties();
+      mainPart.Document.Body.Append(sp);
+    }
+
+    sp.Append(
+      new FooterReference()
+      {
+        Id = footerPartId,
+        Type = HeaderFooterValues.Default,
+      });
+
+    wordDoc.Save();
+  }
 }
