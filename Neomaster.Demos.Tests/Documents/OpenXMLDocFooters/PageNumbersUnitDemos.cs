b3202d2cf5f6860882b707fe2d862e8d885a5589
Neomaster X <neomasterx@outlook.com>
2025-04-13 06:01:04 +0300

    [OpenXML] PageNumbers_Add()

---
+++ Neomaster.Demos.Tests/Documents/OpenXMLDocFooters/PageNumbersUnitDemos.cs
@@ -0,0 +1,57 @@
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Xunit;
+
+namespace Neomaster.Demos.Tests.Documents.OpenXMLDocFooters;
+
+public class PageNumbersUnitDemos
+{
+  [Fact]
+  public void PageNumbers_Add()
+  {
+    const string fileName = "text-001.docx";
+    var templatePath = GetTemplatePath(fileName);
+    var outputPath = Shared.GetOutputPath(fileName);
+
+    File.Copy(templatePath, outputPath, true);
+
+    using var wordDoc = WordprocessingDocument.Open(outputPath, true);
+
+    var mainPart = wordDoc.MainDocumentPart;
+    var footerPart = mainPart.AddNewPart<FooterPart>();
+    var footerPartId = mainPart.GetIdOfPart(footerPart);
+
+    footerPart.Footer = new Footer(
+      new Paragraph(
+        new ParagraphProperties(
+          new Justification() { Val = JustificationValues.Center }),
+        new Run(
+          new FieldChar() { FieldCharType = FieldCharValues.Begin },
+          new FieldCode("PAGE") { Space = SpaceProcessingModeValues.Preserve },
+          new FieldChar() { FieldCharType = FieldCharValues.Separate },
+          new FieldChar() { FieldCharType = FieldCharValues.End })));
+    footerPart.Footer.Save();
+
+    var sp = mainPart.Document.Body.Elements<SectionProperties>().LastOrDefault();
+    if (sp == null)
+    {
+      sp = new SectionProperties();
+      mainPart.Document.Body.Append(sp);
+    }
+
+    sp.Append(
+      new FooterReference()
+      {
+        Id = footerPartId,
+        Type = HeaderFooterValues.Default,
+      });
+
+    wordDoc.Save();
+  }
+
+  private static string GetTemplatePath(string fileName)
+  {
+    return Path.Combine("Documents", "OpenXMLDocFooters", "Templates", fileName);
+  }
+}
