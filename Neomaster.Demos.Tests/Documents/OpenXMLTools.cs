5c8f210f3541e0f057b8ae210d1b9d9a50ab39c1
Neomaster X <neomasterx@outlook.com>
2025-04-13 15:17:48 +0300

    [Open XML] Add watermark: EMU convert methods

--- Neomaster.Demos.Tests/Documents/OpenXMLTools.cs
+++ Neomaster.Demos.Tests/Documents/OpenXMLTools.cs
@@ -6,6 +6,21 @@ namespace Neomaster.Demos.Tests.Documents;
 
 internal static class OpenXMLTools
 {
+  public const int EmusPerInch = 914400;
+  public const int EmusPerCm = 360000;
+  public const int EmusPerPt = 12700;
+  public const int EmusPerPixel = 9525;
+
+  public static long CmToEmu(double cm) => (long)(cm * EmusPerCm);
+  public static long InchesToEmu(double inches) => (long)(inches * EmusPerInch);
+  public static long PtToEmu(double pt) => (long)(pt * EmusPerPt);
+  public static long PxToEmu(int px) => px * EmusPerPixel;
+
+  public static double EmuToCm(long emu) => emu / (double)EmusPerCm;
+  public static double EmuToInches(long emu) => emu / (double)EmusPerInch;
+  public static double EmuToPt(long emu) => emu / (double)EmusPerPt;
+  public static double EmuToPx(long emu) => emu / (double)EmusPerPixel;
+
   public static Hyperlink CreateEmailHyperlink(
     MainDocumentPart mainPart,
     string email,
