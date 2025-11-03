6ea44ef010aaff293e2bff4741bff912857e4112
Neomaster X <neomasterx@outlook.com>
2025-11-03 12:28:59 +0300

    #33 Create 3-level tree.

--- Neomaster.Demos.Tests/LinqExpr/LinqExprUnitDemos.cs
+++ Neomaster.Demos.Tests/LinqExpr/LinqExprUnitDemos.cs
@@ -107,4 +107,41 @@ public class LinqExprUnitDemos()
 
     Assert.Equal(expected, actual);
   }
+
+  [Fact]
+  public void TreeStructure_Create_3Levels()
+  {
+    var leftPar = Expression.Parameter(typeof(Claim));
+    var leftOperand = Expression.Property(leftPar, nameof(Claim.Value));
+    var rightOperand = Expression.Constant("1");
+    var body = Expression.Equal(
+      Expression.Constant(true),
+      Expression.Equal(leftOperand, rightOperand));
+    const string expected =
+      """
+            Constant
+           /
+      Equal      MemberAccess
+           \    /
+            Equal__Constant
+      """;
+
+    var n1 = body.NodeType;
+    var n1L = body.Left.NodeType;
+    var n1R = body.Right.NodeType;
+    var n2 = (BinaryExpression)body.Right;
+    var n2L = n2.Left.NodeType;
+    var n2R = n2.Right.NodeType;
+
+    var actual =
+      $"""
+            {n1L}
+           /
+      {n1}      {n2L}
+           \    /
+            {n1R}__{n2R}
+      """;
+
+    Assert.Equal(expected, actual);
+  }
 }
