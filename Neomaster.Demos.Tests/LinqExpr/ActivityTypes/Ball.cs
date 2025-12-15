890b0ab1f63ca6562642bff6e78cd032797a3a16
Neomaster X <neomasterx@outlook.com>
2025-12-15 20:24:06 +0300

    #34 Ball : IComparable<Ball>

--- Neomaster.Demos.Tests/LinqExpr/ActivityTypes/Ball.cs
+++ Neomaster.Demos.Tests/LinqExpr/ActivityTypes/Ball.cs
@@ -1,6 +1,21 @@
 namespace Neomaster.Demos.Tests.LinqExpr;
 
-public class Ball
+public class Ball : IComparable<Ball>
 {
   public ConsoleColor Color { get; set; }
+
+  public int CompareTo(Ball other)
+  {
+    if (Color > other.Color)
+    {
+      return 1;
+    }
+
+    if (Color < other.Color)
+    {
+      return -1;
+    }
+
+    return 0;
+  }
 }
