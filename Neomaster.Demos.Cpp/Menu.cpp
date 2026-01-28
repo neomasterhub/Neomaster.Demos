2358a628e8b148d6aae366372e69fec9c4dccc88
Neomaster X <neomasterx@outlook.com>
2026-01-28 14:42:33 +0300

    #52 Menu base

---
+++ Neomaster.Demos.Cpp/Menu.cpp
@@ -0,0 +1,8 @@
+#include "Menu.h"
+
+std::vector<MenuItem> Menu::_items =
+{
+  MenuItem("1"),
+  MenuItem("2"),
+  MenuItem("3"),
+};
---
+++ Neomaster.Demos.Cpp/Menu.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "MenuItem.h"
+#include <vector>
+static class Menu
+{
+private:
+  static std::vector<MenuItem> _items;
+};
