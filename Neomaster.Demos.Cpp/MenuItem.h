bdaf117dd89b9c6147f6a6f08293058e60d51a11
Neomaster X <neomasterx@outlook.com>
2026-01-28 14:59:54 +0300

    #52 MenuItem.Action

--- Neomaster.Demos.Cpp/MenuItem.h
+++ Neomaster.Demos.Cpp/MenuItem.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <iostream>
+#include <functional>
 class MenuItem
 {
 public:
   std::string Text;
+  std::function<void()> Action;
 
   MenuItem(
-    std::string text)
+    std::string text,
+    std::function<void()> action)
   {
     Text = text;
+    Action = action;
   }
 };
